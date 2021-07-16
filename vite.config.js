ab4200db980a42c4c2ef0a1e33abd5dd836a6fe5
Jaydan Urwin <jaydan@jaydanurwin.com>
2021-07-15 18:09:09 -0600

    add build config

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   root: 'demos/',
-//   build: {
-//     outDir: '../build',
-//     manifest: true,
-//     rollupOptions: {
-//       input: {
-//         main: resolve(__dirname, 'wc-src/index.html'),
-//       },
-//     },
-//   },
+  build: {
+    outDir: '../build',
+    manifest: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'demos/index.html'),
+      },
+    },
+  },
 });
