f38623bfaae04c1fa00e7d7d127785bd69fc22d4
Jaydan Urwin <jaydan@jaydanurwin.com>
2021-07-20 13:21:21 -0600

    removed css

--- packages/wm-mobile-menu/wm-mobile-menu.ts
+++ packages/wm-mobile-menu/wm-mobile-menu.ts
@@ -1,37 +1,21 @@
-import { LitElement, html, css } from "lit";
+import { LitElement, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
 
 /**
  * An example element.
  *
  * @slot - This element has a slot
- * @csspart button - The button
  */
 
 @customElement("wm-mobile-menu")
 export class MobileMenu extends LitElement {
-  static styles = css`
-    #mobile-menu__container {
-      position: fixed;
-      height: 105vh;
-      width: 100%;
-      z-index: 999;
-      top: 0px;
-      bottom: 0px;
-      left: 0px;
-      right: 0px;
-      background: var(--theme-light-background, #ffffff);
-      color: var(--theme-light-text-on-background, #000000);
-    }
-  `;
 
   @property({ type: Boolean, reflect: true }) isOpen = false;
 
   render() {
     return html`${this.isOpen
-      ? html`<div id="mobile-menu__container">
-          <slot name="mobile-menu"></slot>
-        </div>`
+      ? html`<slot>
+        </slot>`
       : null} `;
   }
 }
