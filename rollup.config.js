476d1be4e63073f05defdf105cdf840365dc0702
Jaydan Urwin <jaydan@jaydanurwin.com>
2021-07-20 13:40:24 -0600

    working scripts and build process

--- rollup.config.js
+++ rollup.config.js
@@ -4,36 +4,9 @@ import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 
 export default [{
-  input: './wc-build/my-element.js',
+  input: 'packages/build/wm-mobile-menu/wm-mobile-menu.js',
   output: {
-    file: './src/assets/components/my-element.bundled.js',
-    format: 'esm',
-  },
-  onwarn(warning) {
-    if (warning.code !== 'THIS_IS_UNDEFINED') {
-      console.error(`(!) ${warning.message}`);
-    }
-  },
-  plugins: [
-    replace({ 'Reflect.decorate': 'undefined', preventAssignment: true }),
-    resolve(),
-    terser({
-      ecma: 2017,
-      module: true,
-      warnings: true,
-      mangle: {
-        properties: {
-          regex: /^__/,
-        },
-      },
-    }),
-    summary(),
-  ],
-},
-{
-  input: './wc-build/mobile-menu.js',
-  output: {
-    file: './src/assets/components/mobile-menu.js',
+    file: 'packages/wm-mobile-menu/build/wm-mobile-menu.bundled.js',
     format: 'esm',
   },
   onwarn(warning) {
