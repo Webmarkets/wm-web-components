892f538534d41e879aa8ad133bb8cc3090f499cc
Jaydan Urwin <jaydan@jaydanurwin.com>
2021-08-05 08:40:33 -0600

    added open modal button

--- demos/src/pages/wm-modal/index.js
+++ demos/src/pages/wm-modal/index.js
@@ -1,11 +1,14 @@
 const WmModal = document.querySelector('wm-modal');
 const modalCloseBtn = document.querySelector('#modal-close__button');
+const modalOpenBtn = document.querySelector('#modal-open__button');
 
 WmModal ? WmModal.addEventListener('clicked-away', (event) => toggleEquipmentModal(event)) : null;
 modalCloseBtn ? modalCloseBtn.addEventListener('click', (event) => toggleEquipmentModal(event)) : null;
+modalOpenBtn ? modalOpenBtn.addEventListener('click', (event) => toggleEquipmentModal(event)) : null;
+
 
 function toggleEquipmentModal(event) {
      event.stopPropagation();
     WmModal.toggleAttribute('isopen');
-    document.body.toggleAttribute('no-scroll');
+    // document.body.toggleAttribute('no-scroll');
 }
