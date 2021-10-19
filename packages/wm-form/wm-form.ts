df75e6ad9c8efd6c86d14e9486f5fb3cf64a8d30
Jaydan Urwin <jaydan@jaydanurwin.com>
2021-10-19 15:29:50 -0600

    style updates

--- packages/wm-form/wm-form.ts
+++ packages/wm-form/wm-form.ts
@@ -1,10 +1,10 @@
-import { html, css, LitElement } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { html, css, LitElement } from "lit";
+import { customElement, property } from "lit/decorators.js";
 
 // this is for Astro
-export const tagName = 'wm-form';
+export const tagName = "wm-form";
 
-@customElement('wm-form')
+@customElement("wm-form")
 export class WebmarketsForm extends LitElement {
   static styles = css`
     .wm-form {
@@ -14,28 +14,33 @@ export class WebmarketsForm extends LitElement {
       font-family: inherit;
     }
 
-    .wm-form input[type='text'],
-    .wm-form input[type='email'],
+    .wm-form input[type="text"],
+    .wm-form input[type="email"],
     .wm-form textarea,
-    .wm-form input[type='tel'],
+    .wm-form input[type="tel"],
     .wm-form select {
+      padding: 0.75rem;
       box-sizing: border-box;
       border: 1px solid #dcdcdc;
       color: var(--wm-form-input-color, #000000);
-      height: 58px;
       width: 100%;
       border-radius: 5px;
-      padding: 10px 20px 10px 20px;
       outline-style: none;
-      -webkit-transition: all 300ms ease-in;
-      transition: all 300ms ease-in;
-      box-shadow: 0 0 0 3px transparent inset;
-      font-size: 18px;
+      /* -webkit-transition: all 300ms ease-in; */
+      /* transition: all 300ms ease-in; */
+      /* box-shadow: 0 0 0 3px transparent inset; */
+      font-size: 1rem;
       font-family: inherit;
+    }
+
+    .wm-form input:focus, .wm-form textarea:focus, .wm-form select:focus {
+        border: 2px solid #000;
     }
 
     .wm-form select {
       color: black;
+      /* Fixes gloss effect on select dropdowns in Safari */
+      -webkit-appearance: none;
     }
 
     .wm-form textarea {
@@ -71,7 +76,7 @@ export class WebmarketsForm extends LitElement {
       display: flex;
     }
 
-    .wm-form input[type='submit'] {
+    .wm-form input[type="submit"] {
       display: inline-block;
       font-size: 1.5rem;
       text-align: center;
@@ -84,7 +89,7 @@ export class WebmarketsForm extends LitElement {
       transition: ease-in-out 350ms;
     }
 
-    .wm-form input[type='submit']:hover {
+    .wm-form input[type="submit"]:hover {
       opacity: 0.8;
       cursor: pointer;
     }
@@ -102,27 +107,27 @@ export class WebmarketsForm extends LitElement {
     }
   `;
 
-  @property({ type: String, reflect: true, attribute: 'formspark-form-id' })
-  formSparkID = '';
+  @property({ type: String, reflect: true, attribute: "formspark-form-id" })
+  formSparkID = "";
 
-  @property({ type: String, reflect: true, attribute: 'redirect-url' })
-  redirectURL = '';
+  @property({ type: String, reflect: true, attribute: "redirect-url" })
+  redirectURL = "";
 
-  @property({ type: String, reflect: true, attribute: 'fields' })
-  fields = 'default';
+  @property({ type: String, reflect: true, attribute: "fields" })
+  fields = "default";
 
-  @property({ type: String, reflect: true, attribute: 'disclaimer' })
-  disclaimer = '';
+  @property({ type: String, reflect: true, attribute: "disclaimer" })
+  disclaimer = "";
 
-  @property({ type: Boolean, reflect: true, attribute: 'full-width-button' })
+  @property({ type: Boolean, reflect: true, attribute: "full-width-button" })
   fullWidthButton = false;
 
-  @property({ type: Array, reflect: true, attribute: 'referral-options' })
+  @property({ type: Array, reflect: true, attribute: "referral-options" })
   referralOptions = [
-    'Google Search',
-    'Family/Friend Referral',
-    'Social Media',
-    'Other',
+    "Google Search",
+    "Family/Friend Referral",
+    "Social Media",
+    "Other",
   ];
 
   _getFormattedDate() {
@@ -132,9 +137,7 @@ export class WebmarketsForm extends LitElement {
 
   render() {
     const referralOptionsElements = this.referralOptions.map((option: any) => {
-      return html`
-      <option value=${option}>${option}</option>
-      `;
+      return html` <option value=${option}>${option}</option> `;
     });
 
     return html`
@@ -152,9 +155,8 @@ export class WebmarketsForm extends LitElement {
           tabindex="-1"
           autocomplete="off"
         />
-        ${
-          this.fields === 'default'
-            ? html`
+        ${this.fields === "default"
+          ? html`
               <div class="w-50">
                 <label for="Name">Name *</label>
                 <input
@@ -188,7 +190,9 @@ export class WebmarketsForm extends LitElement {
               <div class="w-50">
                 <label for="Referral">How did you hear about us? *</label>
                 <select name="Referral" id="contact-location" required>
-                  <option disabled selected>==SELECT AN OPTION==</option>
+                  <option disabled selected value="">
+                    ==SELECT AN OPTION==
+                  </option>
                   ${referralOptionsElements}
                 </select>
               </div>
@@ -201,20 +205,15 @@ export class WebmarketsForm extends LitElement {
                   placeholder="Message"
                 ></textarea>
               </div>
-              ${
-                this.disclaimer
-                  ? html`
-              <div>
-                <p class="disclaimer__text">${this.disclaimer}</p>
-              </div>`
-                  : ''
-              }
+              ${this.disclaimer
+                ? html` <div>
+                    <p class="disclaimer__text">${this.disclaimer}</p>
+                  </div>`
+                : ""}
             `
-            : ''
-        }
-        ${
-          this.fields.includes('name')
-            ? html`
+          : ""}
+        ${this.fields.includes("name")
+          ? html`
               <div>
                 <label for="Name">Name *</label>
                 <input
@@ -226,11 +225,9 @@ export class WebmarketsForm extends LitElement {
                 />
               </div>
             `
-            : ''
-        }
-        ${
-          this.fields.includes('email')
-            ? html`
+          : ""}
+        ${this.fields.includes("email")
+          ? html`
               <div>
                 <label for="Email">Email *</label>
                 <input
@@ -242,11 +239,9 @@ export class WebmarketsForm extends LitElement {
                 />
               </div>
             `
-            : ''
-        }
-        ${
-          this.fields.includes('phone')
-            ? html`
+          : ""}
+        ${this.fields.includes("phone")
+          ? html`
               <div>
                 <label for="Number">Phone Number *</label>
                 <input
@@ -258,24 +253,22 @@ export class WebmarketsForm extends LitElement {
                 />
               </div>
             `
-            : ''
-        }
-        ${
-          this.fields.includes('referral')
-            ? html`
+          : ""}
+        ${this.fields.includes("referral")
+          ? html`
               <div>
                 <label for="Referral">How did you hear about us? *</label>
                 <select name="Referral" id="contact-location" required>
-                  <option disabled selected>==SELECT AN OPTION==</option>
+                  <option disabled selected value="">
+                    ==SELECT AN OPTION==
+                  </option>
                   ${referralOptionsElements}
                 </select>
               </div>
             `
-            : ''
-        }
-        ${
-          this.fields.includes('message')
-            ? html`
+          : ""}
+        ${this.fields.includes("message")
+          ? html`
               <div>
                 <label for="Message">Message *</label>
                 <textarea
@@ -286,31 +279,27 @@ export class WebmarketsForm extends LitElement {
                 ></textarea>
               </div>
             `
-            : ''
-        }
-        ${
-          this.fields.includes('disclaimer') && this.disclaimer
-            ? html`
-        <div>
-          <p class="disclaimer__text">${this.disclaimer}</p>
-        </div>`
-            : ''
-        }
-        ${
-          this.fields === ''
-            ? html`
+          : ""}
+        ${this.fields.includes("disclaimer") && this.disclaimer
+          ? html` <div>
+              <p class="disclaimer__text">${this.disclaimer}</p>
+            </div>`
+          : ""}
+        ${this.fields === ""
+          ? html`
               <div>
                 <p style="color: red; font-size: 1.5rem;">
                   Please provide a valid field value
                 </p>
               </div>
             `
-            : ''
-        }
+          : ""}
 
-        <div class="wm-form-submit__container ${
-          this.fullWidthButton ? 'wm-form-submit__container--wide' : ''
-        }">
+        <div
+          class="wm-form-submit__container ${this.fullWidthButton
+            ? "wm-form-submit__container--wide"
+            : ""}"
+        >
           <input type="submit" title="Submit" />
         </div>
       </form>
