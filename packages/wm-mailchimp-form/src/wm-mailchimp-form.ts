1470490f7bc0151331ac7aa8473733cbf323c915
Jaydan Urwin <jaydan@jaydanurwin.com>
2021-11-02 09:43:09 -0600

    fixed up fields and action url

--- packages/wm-mailchimp-form/src/wm-mailchimp-form.ts
+++ packages/wm-mailchimp-form/src/wm-mailchimp-form.ts
@@ -7,7 +7,7 @@ export class WebmarketsMailchimpForm extends LitElement {
     :host {
       padding: var(--wm-mailchimp-padding, 0);
       border-radius: var(--wm-mailchimp-border-radius, 0);
-      background-color: var(--wm-mailchimp-background, white);
+      background-color: var(--wm-mailchimp-background, #ffffff);
     }
     #title {
       margin: 0 0 0.25rem 0;
@@ -44,7 +44,7 @@ export class WebmarketsMailchimpForm extends LitElement {
     .wm-mailchimp-form input[type="email"] {
       padding: 0.75rem;
       box-sizing: border-box;
-      border: 1px solid #dcdcdc;
+      border: 2px solid #dcdcdc;
       color: var(--wm-form-input-color, #000000);
       width: 100%;
       border-radius: 5px;
@@ -75,9 +75,9 @@ export class WebmarketsMailchimpForm extends LitElement {
       display: inline-flex;
       font-size: 1.2rem;
       line-height: 1;
-      color: var(--wm-mailchimp-background, white);
-      border: 2px solid var(--wm-mailchimp-accent, #39b44a);
-      background-color: var(--wm-mailchimp-accent, #39b44a);
+      color: var(--wm-mailchimp-background, #ffffff);
+      border: 2px solid var(--wm-theme-primary, #0255ff);
+      background-color: var(--wm-theme-primary, #0255ff);
       /* border-radius: 999px; */
       cursor: pointer;
       transition: all 300ms ease-in;
@@ -135,13 +135,9 @@ export class WebmarketsMailchimpForm extends LitElement {
       <div id="mc_embed_signup">
         <p id="title">${this.title}</p>
         <form
-          action=${this.formActionURL}
+          action=${`${this.formActionURL}?u=${this.mailchimpUValue}&amp;id=${this.mailchimpIdValue}`}
           method="POST"
-          id="mc-embedded-subscribe-form"
-          name="mc-embedded-subscribe-form"
-          class="wm-mailchimp-form validate"
-          target="_blank"
-          novalidate
+          class="wm-mailchimp-form"
         >
           <input type="hidden" name="u" value=${this.mailchimpUValue} />
           <input type="hidden" name="id" valu=${this.mailchimpIdValue} />
@@ -159,7 +155,6 @@ export class WebmarketsMailchimpForm extends LitElement {
                     required
                     name=${`MERGE${this.fNameMergeID}`}
                     placeholder="First Name"
-                    class="required"
                     id=${`MERGE${this.fNameMergeID}`}
                   />
                 </div>
@@ -170,15 +165,71 @@ export class WebmarketsMailchimpForm extends LitElement {
                         >Last Name</label
                       >`}
                   <input
-                    type="email"
+                    type="text"
                     value=""
                     required
                     name=${`MERGE${this.lNameMergeID}`}
                     placeholder="Last Name"
-                    class="required email"
                     id=${`MERGE${this.lNameMergeID}`}
                   />
                 </div>
+                <div>
+                  ${this.hideLabels
+                    ? ""
+                    : html`<label for=${`MERGE${this.emailMergeID}`}
+                        >Email Address</label
+                      >`}
+                  <input
+                    type="email"
+                    value=""
+                    required
+                    name=${`MERGE${this.emailMergeID}`}
+                    placeholder="Email Address"
+                    id=${`MERGE${this.emailMergeID}`}
+                  />
+                </div>
+              `
+            : ""}
+          ${this.fields.includes("fname")
+            ? html`
+                <div>
+                  ${this.hideLabels
+                    ? ""
+                    : html`<label for=${`MERGE${this.fNameMergeID}`}
+                        >First Name</label
+                      >`}
+                  <input
+                    type="text"
+                    value=""
+                    required
+                    name=${`MERGE${this.fNameMergeID}`}
+                    placeholder="First Name"
+                    id=${`MERGE${this.fNameMergeID}`}
+                  />
+                </div>
+              `
+            : ""}
+          ${this.fields.includes("lname")
+            ? html`
+                <div>
+                  ${this.hideLabels
+                    ? ""
+                    : html`<label for=${`MERGE${this.lNameMergeID}`}
+                        >Last Name</label
+                      >`}
+                  <input
+                    type="text"
+                    value=""
+                    required
+                    name=${`MERGE${this.lNameMergeID}`}
+                    placeholder="Last Name"
+                    id=${`MERGE${this.lNameMergeID}`}
+                  />
+                </div>
+              `
+            : ""}
+          ${this.fields.includes("email")
+            ? html`
                 <div>
                   ${this.hideLabels
                     ? ""
@@ -197,49 +248,8 @@ export class WebmarketsMailchimpForm extends LitElement {
                 </div>
               `
             : ""}
-          ${this.fields === "email"
-            ? html`
-                 <div>
-                  ${this.hideLabels
-                    ? ""
-                    : html`<label for=${`MERGE${this.emailMergeID}`}
-                        >Email Address</label
-                      >`}
-                  <input
-                    type="email"
-                    value=""
-                    required
-                    name=${`MERGE${this.emailMergeID}`}
-                    placeholder="Email Address"
-                    class="required email"
-                    id=${`MERGE${this.emailMergeID}`}
-                  />
-                </div>
-              `
-            : ""}
-          <!-- 
-            <div id="mce-responses" class="clear">
-              <div
-                class="response"
-                id="mce-error-response"
-                style="display:none"
-              ></div>
-              <div
-                class="response"
-                id="mce-success-response"
-                style="display:none"
-              ></div>
-            </div> -->
-          <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-          <div style="position: absolute; left: -5000px;" aria-hidden="true">
-            <input
-              type="text"
-              name="b_ff4082d1ac23791f7f3fbbb77_c3b9dd0bc6"
-              tabindex="-1"
-              value=""
-            />
-          </div>
-          <div class="clear">
+
+          <div>
             <input
               type="submit"
               value="Subscribe"
@@ -250,27 +260,6 @@ export class WebmarketsMailchimpForm extends LitElement {
           </div>
         </form>
       </div>
-      <script
-        type="text/javascript"
-        src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"
-      ></script>
-      <script type="text/javascript">
-        (function ($) {
-          window.fnames = new Array();
-          window.ftypes = new Array();
-          fnames[0] = "EMAIL";
-          ftypes[0] = "email";
-          fnames[1] = "FNAME";
-          ftypes[1] = "text";
-          fnames[2] = "LNAME";
-          ftypes[2] = "text";
-          fnames[3] = "ADDRESS";
-          ftypes[3] = "address";
-          fnames[4] = "PHONE";
-          ftypes[4] = "phone";
-        })(jQuery);
-        var $mcj = jQuery.noConflict(true);
-      </script>
       <!--End mc_embed_signup-->
     `;
   }
@@ -281,38 +270,3 @@ declare global {
     "wm-mailchimp-form": WebmarketsMailchimpForm;
   }
 }
-
-// Franmetrics CSS
-// #mc_embed_signup {
-//   background: white;
-//   clear: left;
-//   font: 1rem "Poppins", Arial, sans-serif;
-//   border-radius: 1rem;
-//   color: var(--button-color3);
-// }
-// #mc_embed_signup input.button {
-//   font: 1.1em "Poppins";
-//   border-radius: 3rem;
-//   height: auto;
-//   padding: 0.5rem 1.5rem;
-//   display: inline-flex;
-//   text-align: center;
-//   font-size: 1.2rem;
-//   line-height: 1;
-//   padding: 14px 24px;
-//   color: #fff;
-//   border: 2px solid var(--accent-color);
-//   background-color: var(--accent-color);
-//   -webkit-border-radius: 999px;
-//   -moz-border-radius: 999px;
-//   border-radius: 999px;
-//   transition: all 300ms ease-in;
-// }
-// #mc_embed_signup input.button:hover {
-//   background: white;
-//   color: var(--accent-color);
-// }
-// #mc_embed_signup .mc-field-group input {
-//   border-radius: 1rem;
-//   border-color: var(--button-color3);
-// }
