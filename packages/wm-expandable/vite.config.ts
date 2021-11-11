62b4387c928a6c6ff267f26199cf040cfdff6519
Jaydan Urwin <jaydan@jaydanurwin.com>
2021-11-11 15:51:34 -0700

    setup new component

---
+++ packages/wm-expandable/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/wm-expandable.ts',
+      formats: ['es']
+    },
+    rollupOptions: {
+      external: /^lit/
+    }
+  }
+})
