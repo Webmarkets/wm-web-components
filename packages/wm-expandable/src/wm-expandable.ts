7bc11baebc50e35d21124978cef419a9a1cf5cb5
Jaydan Urwin <jaydan@jaydanurwin.com>
2021-11-11 21:08:38 -0700

    solid base for wm-expandable component

--- packages/wm-expandable/src/wm-expandable.ts
+++ packages/wm-expandable/src/wm-expandable.ts
@@ -1,56 +1,87 @@
-import { html, css, LitElement } from 'lit'
-import { customElement, property } from 'lit/decorators.js'
-
+import { html, css, LitElement } from "lit";
+import { customElement, property } from "lit/decorators.js";
+import { expandMoreIcon } from "./icons";
 /**
  * An example element.
  *
  * @slot - This element has a slot
  * @csspart button - The button
  */
-@customElement('wm-expandable')
+@customElement("wm-expandable")
 export class MyElement extends LitElement {
   static styles = css`
-    :host {
-      display: block;
-      border: solid 1px gray;
-      padding: 16px;
-      max-width: 800px;
+
+    .expanded-title__container {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      cursor: pointer;
     }
-  `
+    .expanded-title__container button {
+      margin-right: 0.5rem;
+      padding: 0;
+      background: none;
+      font-size: inherit;
+      border: none;
+      cursor: pointer;
+    }
+    .expand-more__icon {
+      display: flex;
+      transition: transform .3s ease-out;
+    }
+    :host([open]) .expand-more__icon {
+      transform: rotate(180deg);
+    }
+    .expanded-body__container {
+      padding-top: 1rem;
+      display: none;
+      font-size: inherit;
+    }
+    :host([open]) .expanded-body__container {
+      display: flex;
+    }
+  `;
 
   /**
    * The name to say "Hello" to.
    */
-  @property()
-  name = 'World'
+  @property({ type: Boolean, reflect: true, attribute: "open" })
+  isOpen: boolean = false;
 
-  /**
-   * The number of times the button has been clicked.
-   */
-  @property({ type: Number })
-  count = 0
+  firstUpdated() {
+    this.ariaLabel = 'collapsed'
+  }
+
+  public toggleOpen() {
+    if(this.isOpen) {
+      this.ariaLabel = 'collapsed'
+    } else {
+      this.ariaLabel = 'expanded'
+    }
+    this.isOpen = !this.isOpen;
+  }
 
   render() {
     return html`
-      <h1>Hello, ${this.name}!</h1>
-      <button @click=${this._onClick} part="button">
-        Click Count: ${this.count}
-      </button>
-      <slot></slot>
-    `
-  }
-
-  private _onClick() {
-    this.count++
-  }
-
-  foo(): string {
-    return 'foo'
+      <div class="expanded-title__container" @click=${this.toggleOpen}>
+        <slot name="title">
+          <button>Expand Me</button>
+        </slot>
+        <slot name="icon">
+          <span class="expand-more__icon">${expandMoreIcon}</span>
+        </slot>
+      </div>
+      <div class="expanded-body__container">
+        <slot name="body">
+          <p>I'm open!</p>
+        </slot>
+      </div>
+    `;
   }
 }
 
 declare global {
   interface HTMLElementTagNameMap {
-    'my-element': MyElement
+    "wm-expandable": MyElement;
   }
 }
