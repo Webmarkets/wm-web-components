e7f82c3fd55582c6b1c23025805936cbd7dcb5ab
Jaydan Urwin <jaydan@webmarketsonline.com>
2022-05-16 14:23:07 -0600

    added info window to add marker function

--- packages/wm-google-map/src/wm-google-map.ts
+++ packages/wm-google-map/src/wm-google-map.ts
@@ -164,11 +164,25 @@ export class WebmarketsGoogleMap extends LitElement {
     this.loader
       ?.load()
       .then(() => {
-        new google.maps.Marker({
+        const infoWindow = new google.maps.InfoWindow({
+          content: marker.infoWindowContent,
+        })
+        
+        const localMarker = new google.maps.Marker({
           position: { lat: marker.lat, lng: marker.lng },
           map: this.map,
           icon: marker.icon,
         });
+
+        if (marker.infoWindowContent) {
+          localMarker.addListener("click", () => {
+            infoWindow.open({
+              anchor: localMarker,
+              map: this.map,
+              shouldFocus: true,
+            });
+          })
+        }
       })
       .catch((error) => {
         console.log(error);
