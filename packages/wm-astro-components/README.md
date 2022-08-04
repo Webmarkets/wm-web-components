ba4b2888f62d05ba3fcc928d3af2b3f2a65e8d32
EmilyWebmarkets <emily@webmarketsonline.com>
2022-08-04 16:21:24 -0600

    Packaging components for npm

--- packages/wm-astro-components/README.md
+++ packages/wm-astro-components/README.md
@@ -1,3 +1,33 @@
 # webmarkets Astro Components
+A collection of Astro components for use in webmarkets projects.
+
+## Installation
+Run `npm install @webmarkets/wm-astro-components`
+
+## Usage
+You can import a given component in an Astro project with
+```js
+import { Component } from '@webmarkets/wm-astro-components';
+```
 
 ## Components
+
+| Component | Props | Slots |
+| --- | --- | --- |
+| BaseHead | | |
+| BaseBody | | |
+| Button | | |
+| CtaCard | | |
+| Date | | |
+| Image | | |
+| Link | | |
+| Header | | |
+| Hero | | |
+| SectionBuilder | | |
+| TextImageSection | | |
+| TextSection | | |
+| PortableText | | |
+| BlogCategories | | |
+| BlogList | | |
+| BlogPost | | |
+| BlogPostPreview | | |
---
+++ packages/wm-astro-components/components/index.js
@@ -0,0 +1,49 @@
+import BaseBody from './BaseBody.astro';
+
+// Head
+import BaseHead from './BaseHead.astro';
+
+// Generic
+import Button from './generic/Button.astro';
+import CtaCard from './generic/CtaCard.astro';
+import Date from './generic/Data.astro';
+import Image from './generic/Image.astro';
+import Link from './generic/Link.astro';
+
+// Header
+import Header from './header/Header.astro';
+import Hero from './header/Hero.astro';
+
+// Sections
+import SectionBuilder from './sections/SectionBuilder.astro';
+import TextImageSection from './sections/TextImageSection.astro';
+import TextSection from './sections/TextSection.astro';
+
+// Portable Text
+import PortableText from './portableText/PortableText.astro';
+
+// Blog
+import BlogCategories from './blog/BlogCategories.astro';
+import BlogList from './blog/BlogList.astro';
+import BlogPost from './blog/BlogPost.astro';
+import BlogPostPreview from './BlogPostPreview.astro';
+
+export {
+    BaseBody,
+    BaseHead,
+    Button,
+    CtaCard,
+    Date,
+    Image,
+    Link,
+    Header,
+    Hero,
+    SectionBuilder,
+    TextImageSection,
+    TextSection,
+    PortableText,
+    BlogCategories,
+    BlogList,
+    BlogPost,
+    BlogPostPreview
+}
