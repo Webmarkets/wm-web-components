dfedb0b8254f6f20a4f3dbbae22316f8affc54e0
BradleyG248 <calvinandhobbs28@gmail.com>
2024-12-20 17:22:11 -0700

    mailchimp

--- packages/wm-mailchimp-form/vite.config.ts
+++ packages/wm-mailchimp-form/vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
-      entry: 'src/wm-mailchimp-form.ts',
-      formats: ['es'],
+      entry: "src/wm-mailchimp-form.ts",
+      // formats: ['es'],
     },
     // rollupOptions: {
     //   external: /^lit/,
